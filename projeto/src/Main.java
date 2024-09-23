bcaaa02c2faf29262842bd7dd5f46455a87fe2eb
Maycon Pedroso Macario <160797266+Basara24@users.noreply.github.com>
2024-09-23 05:30:30 -0300

    mexida no main

--- projeto/src/Main.java
+++ projeto/src/Main.java
@@ -1,18 +1,23 @@
+import com.google.gson.Gson;
 import model.Cena;
 import model.Item;
 import model.Save;
 import repositorio.CenaDAO;
 import repositorio.ItemDAO;
 import repositorio.SaveDAO;
+import spark.Spark;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
+    private static  final Gson Gson = new  Gson();
     public static void main(String[] args) {
-        System.out.println();
 
         try {
+            Save save = SaveDAO.novoJogo();
+            String saveJson = Gson.toJson(save);
+            Spark.get("/", (req, res) -> saveJson);
 
         } catch (Exception e) {
             throw new RuntimeException(e);
